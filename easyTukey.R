b049613e9901a82832f45476413a31e2a05a7ebc
illumination-k <43526984+illumination-k@users.noreply.github.com>
2021-07-12 17:32:45 +0900

    Update easyTukey.R

--- easyTukey.R
+++ easyTukey.R
@@ -15,6 +15,7 @@ tukeyFromFile <- function(path, sep="\t") {
     df['condition'] <- c(1:length(rownames(df)))
     mergedDf <- df[c(colnames(df)[1], 'condition')]
     for (i in colnames(df)[1:length(colnames(df))-1]) {
+        if (i == "V1") { next }
         tmp <- df[c(i, "condition")]
         colnames(tmp) <- c(colnames(df)[1], "condition")
         mergedDf <- rbind(mergedDf, tmp)
